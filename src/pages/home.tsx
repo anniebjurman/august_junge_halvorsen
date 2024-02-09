4c2f83c6412fc7d1bd2322209d45a279aede6327
Annie Bjurman <id19abn@cs.umu.se>
2024-02-09 11:19:43 +0100

    header

---
+++ src/pages/home.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './home.css';
+
+function Home() {
+  return (
+    <div className="home">
+      <header className="header">
+        <div className='h_part h_start'>
+            <h1>August</h1>
+        </div>
+        <div className='h_part h_middle'>
+            <h1>Junge</h1>
+        </div>
+        <div className='h_part h_end'>
+            <h1>Halvorsen</h1>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
