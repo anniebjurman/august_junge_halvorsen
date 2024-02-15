5eb5674109af0072b9b699acf6f9f31d230c3853
Annie Bjurman <id19abn@cs.umu.se>
2024-02-15 16:07:28 +0100

    update

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
 import './Carousel.css';
+import { motion } from 'framer-motion';
+import CrossfadeImage from "react-crossfade-image";
 
 function Carousel() {
     const images = [
-        './images/img2.jpeg',
-        '/images/img3.jpeg',
-        '/images/img1.jpeg'
+        './images/August_Junge_Halvorsen_01.jpg',
+        '/images/August_Junge_Halvorsen_02.jpg',
+        '/images/August_Junge_Halvorsen_03.jpg'
     ];
 
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -19,9 +21,21 @@ function Carousel() {
       }, [images.length]);
 
     return (
-        <div className='carousel'>
-            <img src={images[currentImageIndex]} alt='design of houses' />
+      <>
+        <div className='carousel_desktop'>
+            <CrossfadeImage
+              src={images[currentImageIndex]}
+              style={{aspectRatio: '3/4'}}
+            />
         </div>
+
+        <div className='carousel_mobile'>
+            <CrossfadeImage
+              src={images[currentImageIndex]}
+              style={{minHeight: '102vh', objectFit: 'cover' }}
+            />
+        </div>
+      </>
     );
 }
 
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,10 +2,11 @@ import './Home.css';
 import Carousel from '../components/Carousel';
 
 function Home() {
+
   return (
     <div className="home">
 
-      <header className="header">
+      <div className="header_desktop">
         <div className='h_part h_start'>
             <h1>August</h1>
         </div>
@@ -15,7 +16,13 @@ function Home() {
         <div className='h_part h_end'>
             <h1>Halvorsen</h1>
         </div>
-      </header>
+      </div>
+
+      <div className='header_mobile'>
+        <h1>August</h1>
+        <h1>Junge</h1>
+        <h1>Halvorsen</h1>
+      </div>
 
       <div className='body'>
         <Carousel />
